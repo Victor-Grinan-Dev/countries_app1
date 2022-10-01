8c4e333d709dbf19c0233fcf412454696fb2d221
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-10-01 19:01:39 +0300

    extra files erased

--- src/App.jsx
+++ src/App.jsx
@@ -49,8 +49,6 @@ const App = () => {
     if(initialData){
       tempArray = initialData.split(',');   
       dispatch(setFavorites(tempArray));
-    }else{
-      console.log("empty local storage");
     }
   }, [dispatch, countries]);
 
