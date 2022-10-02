e99efff5ef1697ed68049823df8e54b7007b987d
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-10-02 11:45:17 +0300

    added delete button to favorites

--- src/components/UIs/FavCard.jsx
+++ src/components/UIs/FavCard.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-
-
+import { useDispatch } from 'react-redux';
+import { deleteFromFavorite } from '../../features/countries/countriesSlice';
+import { Button } from 'react-bootstrap';
 
 function FavCard({name}) {
+    const dispatch = useDispatch();
   return (
-    <div className='favCard' >{name}</div>
+    <div className='favCard' >
+        <p> { name } </p> <Button onClick={()=>dispatch(deleteFromFavorite(name))}> x </Button>
+    </div>
   )
 }
 
