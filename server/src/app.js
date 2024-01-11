0a6ed984b8481b885b5c86d7b0e941d20fa00eca
Victor-python <64462520+Victor-python@users.noreply.github.com>
2024-01-11 11:30:56 +0200

    vercel .gitignore

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 const countriesRouter = require("./routes/countries/countries.router");
 const favCountriesRouter = require("./routes/favCountries/favCountries.router");
 const filterCountriesRouter = require("./routes/countriesFilter/countriesfilter.router");
@@ -9,11 +10,14 @@ const app = express();
 
 /*** middlewear***/
 app.use(express.json());
+
 app.use(
   cors({
     origin: "http://localhost:3000",
   })
 );
+app.use(morgan("combined"));
+
 app.use(express.static(path.join(__dirname, "..", "public")));
 
 //routes
