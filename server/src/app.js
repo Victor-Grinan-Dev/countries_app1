830273ed3cf306294da59bfbc605867dca7edfd0
Victor-python <64462520+Victor-python@users.noreply.github.com>
2024-01-11 12:04:24 +0200

    add cors options

--- server/src/app.js
+++ server/src/app.js
@@ -6,16 +6,27 @@ const countriesRouter = require("./routes/countries/countries.router");
 const favCountriesRouter = require("./routes/favCountries/favCountries.router");
 const filterCountriesRouter = require("./routes/countriesFilter/countriesfilter.router");
 
+const corWhiteList = [
+  "https://countries-app-five-mu.vercel.app/",
+  "http://localhost:3000",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (corWhiteList.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
 const app = express();
 
 /*** middlewear***/
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+app.use(cors(corsOptions));
 app.use(morgan("combined"));
 
 app.use(express.static(path.join(__dirname, "..", "public")));
